5893156f7823d995e2fea76cc28f0d18fff82acd
KSD Developer <ksapp@aok-net.com>
2017-01-20 16:25:06 +0900

    fix version.

--- version.go
+++ version.go
@@ -9,7 +9,7 @@ import (
 const Name = "offer"
 
 // Version is application version
-const Version string = "v0.0.8"
+const Version string = "v0.0.1"
 
 func OutputVersion() string {
 	var buf bytes.Buffer
