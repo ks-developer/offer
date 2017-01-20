545719f0922605eaf4390bdb040161fdddf593b2
KSD Developer <ksapp@aok-net.com>
2017-01-20 14:30:29 +0900

    add release_tag file.

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # offer
 
-[![CircleCI](https://circleci.com/gh/ks-developer/offer/tree/master.svg?style=svg)](https://circleci.com/gh/ks-developer/offer/tree/master)
+## Status
 
-```shell
->offer -u 1
+[![Circle CI](https://circleci.com/gh/ks-developer/offer/tree/master.svg?style=shield)](https://circleci.com/gh/ks-developer/offer/tree/master)
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+## Usage
+
+```shell-script
+offer -u 1
 4533DD1D
->offer -d -u 4533DD1D
+offer -d -u 4533DD1D
 1
 ```
--- cmd/offer/version.go
+++ cmd/offer/version.go
@@ -9,41 +9,12 @@ import (
 const Name = "offer"
 
 // Version is application version
-const Version string = "v0.0.1"
+const Version string = "v0.0.4"
 
 func OutputVersion() string {
 	var buf bytes.Buffer
 	fmt.Fprintf(&buf, "%s version %s", Name, Version)
 	fmt.Fprintf(&buf, "\n")
-
-	// Check latest version is release or not.
-	// verCheckCh := make(chan *latest.CheckResponse)
-	// go func() {
-	// 	fixFunc := latest.DeleteFrontV()
-	// 	githubTag := &latest.GithubTag{
-	// 		Owner:             "ks-developer",
-	// 		Repository:        "offer",
-	// 		FixVersionStrFunc: fixFunc,
-	// 	}
-
-	// 	res, err := latest.Check(githubTag, fixFunc(Version))
-	// 	if err != nil {
-	// 		// Don't return error
-	// 		Debugf("[ERROR] Check lastet version is failed: %s", err)
-	// 		return
-	// 	}
-	// 	verCheckCh <- res
-	// }()
-
-	// select {
-	// case <-time.After(defaultCheckTimeout):
-	// case res := <-verCheckCh:
-	// 	if res.Outdated {
-	// 		fmt.Fprintf(&buf,
-	// 			"Latest version of ghr is v%s, please upgrade!\n",
-	// 			res.Current)
-	// 	}
-	// }
 
 	return buf.String()
 }
